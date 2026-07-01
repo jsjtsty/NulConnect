f3ffd7860eee7474ff18c64e47f4e76e9d627ca7
nullsty <jsjtsty@126.com>
2026-07-02 01:26:51 +0800

    Initial Commit

---
+++ NulConnectTests/NulConnectTests.swift
@@ -0,0 +1,19 @@
+//
+//  NulConnectTests.swift
+//  NulConnectTests
+//
+//  Created by 孙天阳 on 2026/7/2.
+//
+
+import Testing
+@testable import NulConnect
+
+struct NulConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
