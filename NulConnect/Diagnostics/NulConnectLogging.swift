82e22d2fbecb6ddb5161a9da364baad1e4288068
nullsty <jsjtsty@126.com>
2026-08-09 04:23:17 +0800

    Disable diagnostic logging by default

--- NulConnect/Diagnostics/NulConnectLogging.swift
+++ NulConnect/Diagnostics/NulConnectLogging.swift
@@ -1,7 +1,11 @@
 import Foundation
 
+#if DEBUG && NULCONNECT_ENABLE_LOGS
 @inline(__always)
 nonisolated func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    let message = items.map { String(describing: $0) }.joined(separator: separator)
-    NulConnectDiagnostics.log(message + (terminator == "\n" ? "" : terminator))
+    Swift.print(items.map { String(describing: $0) }.joined(separator: separator), terminator: terminator)
 }
+#else
+@inline(__always)
+nonisolated func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {}
+#endif
